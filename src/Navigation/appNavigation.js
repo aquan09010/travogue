4c395f2d485e23f785fa165b2a98c0ea05ea60af
aquan09010 <quan.caoaquan09010@hcmut.edu.vn>
2024-04-12 22:57:55 +0700

    change sdk to 49 && remove react-native-reanimated react-native-gesture-handler

--- src/Navigation/appNavigation.js
+++ src/Navigation/appNavigation.js
@@ -12,7 +12,7 @@ import ConfirmPhoneScreen from "@/Screens/ConfirmPhoneScreen";
 import CreatePasswordScreen from "@/Screens/CreatePasswordScreen";
 import HostProfile from "@/Screens/HostProfile";
 import SearchCityScreen from "@/Screens/SearchCityScreen";
-import WhereaboutSearchScreen from "@/Screens/WhereaboutSearchScreen";
+// import WhereaboutSearchScreen from "@/Screens/WhereaboutSearchScreen";
 
 const Stack = createNativeStackNavigator();
 export default function AppNavigation() {
@@ -88,11 +88,11 @@ export default function AppNavigation() {
           options={{ headerShown: false }}
           component={SearchCityScreen}
         />
-        <Stack.Screen
+        {/* <Stack.Screen
           name="WhereaboutSearch"
           options={{ headerShown: false }}
           component={WhereaboutSearchScreen}
-        />
+        /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
