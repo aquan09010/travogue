98b785cbce0d24fc79a1c308f52dbb3cd5735ae5
Nguyen Lam Tung <tung.nguyen.mauxox@gmail.com>
2024-04-27 18:16:33 +0700

    Add more fonts

--- src/index.jsx
+++ src/index.jsx
@@ -11,6 +11,16 @@ export default function App() {
     BeVN: require("../assets/font/BeVietnamPro-Regular.ttf"),
     BeVNBold: require("../assets/font/BeVietnamPro-Bold.ttf"),
     BeVNSemi: require("../assets/font/BeVietnamPro-SemiBold.ttf"),
+    FontAwesome6ProLight: require('../assets/fonts/FontAwesome6Pro-Light.otf'),
+    FontAwesome6FreeRegular: require('../assets/fonts/FontAwesome6Free-Regular.otf'),
+    BeVNProLight: require('../assets/fonts/BeVietnamPro-Light.ttf'),
+    BeVNProMedium: require('../assets/fonts/BeVietnamPro-Medium.ttf'),
+    BeVNProThinItalic: require('../assets/fonts/BeVietnamPro-ThinItalic.ttf'),
+    BeVNProExtraLight: require('../assets/fonts/BeVietnamPro-ExtraLight.ttf'),
+    UrbanistBold: require('../assets/fonts/Urbanist-Bold.ttf'),
+    UrbanistRegular: require('../assets/fonts/Urbanist-Regular.ttf'),
+    VogueRegular: require('../assets/fonts/Vogue-Regular.otf'),
+    SFProTextLight: require('../assets/fonts/SFProText-Light.otf')
   });
 
   if (!loaded) return null;
